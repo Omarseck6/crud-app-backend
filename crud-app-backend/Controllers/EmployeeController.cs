f819ba8ef410d2f9dbf364fb90f0e73846efe9a2
Omar SECK <seckomar839@gmail.com>
2024-12-05 19:40:06 +0000

    Ajoutez des fichiers projet.

---
+++ crud-app-backend/Controllers/EmployeeController.cs
@@ -0,0 +1,49 @@
+﻿using crud_app_backend.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace crud_app_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeRepository _employeeRepository;
+
+        public EmployeeController(EmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+        [HttpPost]
+        public async Task<ActionResult> AddEmployee([FromBody] Employee model)
+        {
+          await  _employeeRepository.AddEmployeeAsync(model);
+            return Ok();
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetEmployeeList()
+        {
+            var employeList = await _employeeRepository.GetAllEmployeeAsync();
+            return Ok(employeList);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetEmployeeById([FromRoute] int id)
+        {
+            var employe = await _employeeRepository.GetEmployeeById(id);
+            return Ok(employe);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateEmployee([FromRoute] int id, [FromBody] Employee model)
+        {
+            await _employeeRepository.UpdateEmployee(id, model);
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteEmployee([FromRoute] int id)
+        {
+            await _employeeRepository.DeleteEmployee(id);
+            return Ok();
+        }
+    }
+}
---
+++ crud-app-backend/Data/EmployeeRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace crud_app_backend.Data
+{
+    
+    public class EmployeeRepository
+
+    {
+        private readonly AppDbContext _appDbContext;
+        public EmployeeRepository(AppDbContext appDbContext) {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task AddEmployeeAsync(Employee employee) {
+            await _appDbContext.Set<Employee>().AddAsync(employee);
+            await _appDbContext.SaveChangesAsync();
+        }
+        public async Task<List<Employee>> GetAllEmployeeAsync()
+        {
+          return  await _appDbContext.Employees.ToListAsync();
+        }
+
+        public async Task<Employee> GetEmployeeById(int id)
+        {
+            return await _appDbContext.Employees.FindAsync(id);
+        }
+
+        public async Task UpdateEmployee(int id, Employee model)
+        {
+            var employee = await _appDbContext.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                throw new Exception("Employee not found");
+            }
+            employee.Name = model.Name;
+            employee.Phone = model.Phone;
+            employee.Age = model.Age;
+            employee.Salary = model.Salary;
+            await _appDbContext.SaveChangesAsync();
+
+        }
+
+        public async Task DeleteEmployee(int id)
+        {
+            var employee = await _appDbContext.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                throw new Exception("Employee not found");
+            }
+            _appDbContext.Employees.Remove(employee);
+            await _appDbContext.SaveChangesAsync();
+        }
+    }
+}
